5f6f7f6d86fce178c1583cb097f683cb0c3c6ec9
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2019-10-17 20:42:12 +0100

    corrected tabs

--- VikImg.java
+++ VikImg.java
@@ -1,9 +1,9 @@
 public class VikImg {
 
-	public static void main(String[] args) {
-		byte[] bytes = {10, 10, 10, 'h', 'e', 'l', 'l', 'o'};
-		boolean ok = Writer.write("myfile.txt", bytes);
-		if (!ok) System.out.println("Something went wrong.");
-	}
+    public static void main(String[] args) {
+        byte[] bytes = {10, 10, 10, 'h', 'e', 'l', 'l', 'o'};
+        boolean ok = Writer.write("myfile.txt", bytes);
+        if (!ok) System.out.println("Something went wrong.");
+    }
 
 }
--- Writer.java
+++ Writer.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 
 public class Writer {
 
-	public static boolean write(String filepath, byte[] bytes) {
-		File file = new File(filepath);
-		try ( FileOutputStream fos = new FileOutputStream(file) ) {
-			fos.write(bytes);
-			return true;
-		} catch (IOException e) {
-			return false;
-		}
-	}
+    public static boolean write(String filepath, byte[] bytes) {
+        File file = new File(filepath);
+        try ( FileOutputStream fos = new FileOutputStream(file) ) {
+            fos.write(bytes);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
 
 }
