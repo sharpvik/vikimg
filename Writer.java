0efb3f3530a6ce3e9d2095d1d0a34666c30aee24
V.A.Rozenko Voitenko <vr4n18@l10.clients.soton.ac.uk>
2019-10-17 20:36:30 +0100

    first commit

---
+++ Writer.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Writer {
+
+	public static boolean write(String filepath, byte[] bytes) {
+		File file = new File(filepath);
+		try ( FileOutputStream fos = new FileOutputStream(file) ) {
+			fos.write(bytes);
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+
+}
